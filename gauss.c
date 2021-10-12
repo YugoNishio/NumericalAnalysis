07cf0cca7db4f4e98a2e6675430d215839de4f1b
YugoNishio <s20C1089DK@s.chibakoudai.jp>
2021-10-13 01:15:24 +0900

    add

--- gauss.c
+++ gauss.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
-double x1, x2, x3, error;
-double before_x1, before_x2, before_x3;
-double error1, error2, error3;
-int roop;
+double x1, x2, x3, error; //変数と収束誤差
+double before_x1, before_x2, before_x3; //一つ前の変数値
+double error1, error2, error3; //一つ前の変数値との誤差
+int roop; //ループカウント用変数
 
 double get_x1(double x2, double x3){
     return((10.0 - x2 -x3) / 5.0);
@@ -19,25 +19,25 @@ double get_x3(double x1, double x2){
 }
 
 void get_roop(){
-    x1 = x2 = x3 = 1.0; //initial values
-    before_x1 = before_x2 = before_x3 = 0.0;
-    error = 0.0001;
+    x1 = x2 = x3 = 1.0; //変数の初期値
+    before_x1 = before_x2 = before_x3 = 0.0; //errorに引っ掛からない程度の値
+    error = 0.0001; //誤差0.0001とする
     roop = 0;
 
     while(1){
-        error1 = fabs(x1 - before_x1);
+        error1 = fabs(x1 - before_x1); //変数と一つ前の変数の差
         error2 = fabs(x2 - before_x2);
         error3 = fabs(x3 - before_x3);
-        before_x1 = x1;
+        before_x1 = x1; //変数の計算前に一つ前の変数に代入
         before_x2 = x2;
         before_x3 = x3;
-        x1 = get_x1(x2, x3);
+        x1 = get_x1(x2, x3); //変数の計算
         x2 = get_x2(x1, x3);
         x3 = get_x2(x1, x2);
-        roop += 1;
+        roop += 1; //ループカウント
 
         printf("[i = %3d] x1 = %.10f, x2 = %.10f, x3 = %.10f\n", roop, x1, x2, x3);
-        if((error1 < error) && (error2 < error) && (error3 < error)){
+        if((error1 < error) && (error2 < error) && (error3 < error)){ //収束条件
             printf("収束\n終了します\n");
             break;
         }
@@ -45,6 +45,6 @@ void get_roop(){
 }
 
 int main(){
-    get_roop();
+    get_roop(); //実行
     return 0;
 }
