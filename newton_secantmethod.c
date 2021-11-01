6ce5fbe8a5a735e9f554e2a66cdeac7eb8409d42
YugoNishio <s20C1089DK@s.chibakoudai.jp>
2021-11-02 07:03:55 +0900

    completion

--- newton_secantmethod.c
+++ newton_secantmethod.c
@@ -1,37 +1,38 @@
-//ogawaのコードを参考に作成
 #include <stdio.h>
 #include <math.h>
 
-double delta, x, next_x, a;
+double delta, x, a;
+double h;
 int n;
 
-#define eps 0.000001 //
-#define n_max 100 //最大実行回数
+#define eps 0.000001 //最小値
+#define n_max 150 //最大実行回数
 
 double f(double x){
-    return x*x*x -3*x*x +9*x -8;
+    //return 2/(x*x) +x -10;
+    //return x + cos(x);
+    return 5*x*x*x -1*x*x +9*x -10;
 }
 
 double df(double x){
-    double h = 0.000001;
+    h = 0.000001;
 	return (f(x+h)-f(x))/h;
 }
 
 double newton(double x_n){
     n = 0;
-    x = x_n;
+    x = x_n;//初期値
     printf("%f\n", x);
     do{
     delta = (-1 * f(x)) / df(x);
     x += delta;
-    printf("%3d x=%lf\n", n, x);
+    printf("%lf\n", x);
     n++;
     }
-    while((fabs(delta) > eps) && (n <= n_max));
-    return next_x;
+    while((fabs(delta) > eps) && (n < n_max));//収束条件
 }
 
 int main(){
-    a = newton(10);
+    a = newton(10);//初期値設定
     return 0;
 }
